d6cf96d0deebbdc8e374bb7fdb91787498894c6f
publius_cp <SND\publius_cp>
2008-07-07 00:58:49 +0000

    a windows installer added to the build. crashme.c has code to find its own exe in search path.

--- build.bat
+++ build.bat
@@ -5,6 +5,15 @@
 @PATH %PATH%;%WIXBIN%;%NETBIN%;%VS8BIN%
 @SET T=Release
 @MSBUILD CRASHME.SLN /Target:Build /p:Configuration=Release
+@
+@IF EXIST CRASHME.WIXOBJ DEL CRASHME.WIXOBJ
+@CANDLE CRASHME.WXS -nologo
+@ECHO Linking CRASHME installer.
+@IF EXIST CRASHME.MSI DEL CRASHME.MSI
+@REM Note: WixUI_en-us.wxl might be in your wix-2.0.4820.0-binaries
+@REM       and not in the %WIXBIN% as installed.
+@LIGHT CRASHME.WIXOBJ -nologo "%WIXBIN%\WixUI.wixlib" -loc "%WIXBIN%\WixUI_en-us.wxl" -out CRASHME.MSI
+@Echo Build Done
 
 
 
